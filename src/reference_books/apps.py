b02e9f1b91580e49666e0599e480f474d40ec25a
Kurbangaleev Ilnur <haker_ilnur_97@mail.ru>
2023-02-22 15:27:40 +0500

    Добавил модели в админку.

--- src/reference_books/apps.py
+++ src/reference_books/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ReferenceBooksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reference_books'
+    verbose_name = 'Медицинский справочник'
