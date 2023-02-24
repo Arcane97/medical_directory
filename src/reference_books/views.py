15e1ec6d0c7ede6a9e99f7992fecbd8f5185053d
ilnur <haker_ilnur_97@mail.ru>
2023-02-24 20:55:48 +0500

    Привёл ответ вьюшки ReferenceBookListView к формату как в примере {"refbooks": [{},{}] }.

--- src/reference_books/views.py
+++ src/reference_books/views.py
@@ -1,12 +1,21 @@
-from rest_framework import mixins
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from . import models
 from . import serializers
 
 
-class ReferenceBookListView(mixins.ListModelMixin, GenericViewSet):
+class ReferenceBookListView(GenericViewSet):
     queryset = models.ReferenceBook.objects.all()
     serializer_class = serializers.ReferenceBookSerializer
-    # todo привести ответ к формату как в примере {"refbooks": [{},{}] }
+
     # todo фильтрация с date
+
+    def list(self, request, *args, **kwargs):
+        """
+        Если нужен будет пагинатор, убрать этот метод и в пагинаторе, вместо "results", указать "refbooks".
+        """
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        data = {"refbooks": serializer.data}
+        return Response(data)
